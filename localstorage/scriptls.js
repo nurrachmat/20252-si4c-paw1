dfea049fee5bbe2709c5385c384161cd095f9a1e
Nur Rachmat <rachmat.nur@gmail.com>
2026-04-07 13:00:07 +0700

    bootstrap + web storage

--- localstorage/scriptls.js
+++ localstorage/scriptls.js
@@ -1,5 +1,6 @@
 let npm = document.getElementById("npm");
 let nama = document.getElementById("nama");
+let image_url = document.getElementById("image_url");
 
 function simpan() {
     console.log(npm.value)
@@ -21,7 +22,8 @@ function simpan() {
     // simpan value npm dan nama ke dalam object data
     data.push({
         npm: npm.value, 
-        nama: nama.value
+        nama: nama.value,
+        image_url: image_url.value
     })
     console.log(data)
 
@@ -43,7 +45,15 @@ function tampil() {
     // lakukan perulangan (forEach)
     hasil.forEach(element => {
         // console.log(element)
-        document.getElementById("list-mhs").innerHTML += `<li>${element.npm} ${element.nama}</li>`
+        document.getElementById("list-mhs").innerHTML += `<div class="col-lg-4 col-md-6 mt-3">
+            <div class="card">
+                <img src="${element.image_url}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${element.nama}</h5>
+                    <p class="card-text">${element.npm}</p>
+                </div>
+            </div>
+        </div>`
     });
 }
 
